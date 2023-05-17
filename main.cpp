609db3ef35f2189877838e1d1a38c7c7b5eebc9a
PaponJob <132637951+PaponJob@users.noreply.github.com>
2023-05-17 09:04:30 +0000

    Pointer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#include <iomanip>
 //1. Creating make file
 //2. Swap the value in the array using pointers** to help
 //3. Take input from argv
@@ -8,32 +9,32 @@ using namespace std;
 int main(int argc, char **argv){
 
   
-/* int i,N,*a;
-			N=argc-1;
- 		     a=new int[N];
-			for(i=0;i<N;i++) {
-			    a[i]=atoi(argv[i]);
+ int i,n,*a;
+			n=argc-1;
+ 		     a=new int[n];
+			for(i=0;i<n;i++) {
+			    a[i]=atoi(argv[i+1]);
 		         cout<< "a[" << i << "]=" <<a[i]<<endl;
-		}*/
+		}
 
-//void main(){
-      int n,*p,i;
-    cout<<"How many numbers needed?";
-    cin>> n;
-	   p=new int[n];
+  int*pa=a;
+  int*pb=a+n-1;
+  int temp;
+  
+for (int i = 0; i < n / 2; i++) {
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+    pa++; pb--;
+}
+  pa-=n/2;
 
- for(i=0;i<n;i++) {
- 		   cin>>*p;//p is the location where p points to
-      p++;  
- }
- p-=n; //rewind the p to the starting location
-     // Using p within your program 
-  for(i=0;i<n;i++) {
- 		   cout<<p[i]<<" "; //p is the location where p points to
- //     p++;  
- }
+for (int i = 0; i < n - 1; i++,pa++)
+cout<<*pa<<" ";
+  
+cout<<*pa<<endl;
 
- delete [] p;  
+ delete [] a;  
 }
 
   
